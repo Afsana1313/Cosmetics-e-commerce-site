6a40c5d15c05ea394d03672177fc8abe1a3a6f32
Afsana Zaman Nipa <afsana2013nipa@gmail.com>
2021-10-02 00:00:20 +0600

    add to cart

--- pages/components/SingleProductTable.tsx
+++ pages/components/SingleProductTable.tsx
@@ -6,12 +6,13 @@ type GetProps = {
         brand: string
         api_featured_image: string
         product_type: string
+        price: number
     }
 }
 const SingleProductTable = ({data}: GetProps) => {
     return (
         <div className='single-product-item'>
-                                        
+                {console.log(data)}                      
             <div className='single-product-item-img'>
                 <img
                     src={data?.api_featured_image}
@@ -22,17 +23,23 @@ const SingleProductTable = ({data}: GetProps) => {
                 <div >
                     <span
                         style={{fontSize: 20, fontWeight: 500}}
-                    >{data?.name as string}</span>
+                    >{data?.name}</span>
                 </div>
                 <div >
                     <span
                         style={{fontSize: 18}}
                     >{data?.brand}</span>
                 </div>
+                <div>
+                    <span>${data?.price}</span>
+                </div>
                 <div >
                     <span>{data?.product_type}</span>
                 </div>   
-        </div> 
+            </div>
+            <div className='single-product-add-to-cart'>
+                    <span>Add to cart</span>
+            </div>
         </div>
     )
 }
